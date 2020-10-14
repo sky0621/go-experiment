7970e92f37ba0d5dbf369b880d24231970172f87
sky0621 <sky14work@gmail.com>
2020-10-14 21:23:44 +0900

    v1

--- src/main.go
+++ src/main.go
@@ -20,55 +20,13 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/add-school", schoolHandler(project, "add-school"))
-	e.GET("/edit-school/:sid", schoolHandler(project, "edit-school"))
-
-	e.GET("/add-grade/:sid", handler(project, "add-grade"))
-	e.GET("/add-class/:sid", handler(project, "add-class"))
-
-	e.GET("/add-teacher/:sid", handler(project, "add-teacher"))
-	e.GET("/add-student/:sid", handler(project, "add-student"))
+	e.GET("/add-school", handler(project, "add-school"))
+	e.GET("/add-grade", handler(project, "add-grade"))
+	e.GET("/add-class", handler(project, "add-class"))
+	e.GET("/add-teacher", handler(project, "add-teacher"))
+	e.GET("/add-student", handler(project, "add-student"))
 
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func schoolHandler(project, path string) func(c echo.Context) error {
-	return func(c echo.Context) error {
-		ctx := c.Request().Context()
-
-		client, err := firestore.NewClient(ctx, project)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer client.Close()
-
-		operationSequence := createOperationSequence()
-
-		schoolID := c.Param("sid")
-		if schoolID == "" {
-			schoolID = createUUID()
-
-			_, err = client.Collection("school").Doc(schoolID).
-				Set(ctx, map[string]interface{}{
-					"state": "ACTIVE",
-				}, firestore.MergeAll)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		_, err = client.Collection("school").Doc(schoolID).
-			Collection("operation").Doc(fmt.Sprintf("%d", operationSequence)).
-			Set(ctx, map[string]interface{}{
-				"operationSequence": operationSequence,
-				"order":             path + ":" + schoolID,
-			}, firestore.MergeAll)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return c.String(http.StatusOK, schoolID)
-	}
 }
 
 func handler(project, path string) func(c echo.Context) error {
@@ -81,17 +39,11 @@ func handler(project, path string) func(c echo.Context) error {
 		}
 		defer client.Close()
 
-		schoolID := c.Param("sid")
-		if schoolID == "" {
-			log.Fatal("no schoolID")
-		}
-
 		newID := createUUID()
 
-		operationSequence := createOperationSequence()
+		operationSequence := time.Now().UnixNano()
 
-		_, err = client.Collection("school").Doc(schoolID).
-			Collection("operation").Doc(fmt.Sprintf("%d", operationSequence)).
+		_, err = client.Collection("operation").Doc(fmt.Sprintf("%d", operationSequence)).
 			Set(ctx, map[string]interface{}{
 				"operationSequence": operationSequence,
 				"order":             path + ":" + newID,
@@ -109,8 +61,4 @@ func createUUID() string {
 		log.Fatal(err)
 	}
 	return u.String()
-}
-
-func createOperationSequence() int64 {
-	return time.Now().UnixNano()
 }
